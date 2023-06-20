f6da0c1d55472d2aee415949a319d0fc1e3dd0fb
abdulkosim1 <kasim2509@mail.ru>
2023-06-20 19:22:41 +0600

    a

--- svetofor.py
+++ svetofor.py
@@ -101,26 +101,106 @@
 
 # task4
 # import requests
+# from bs4 import BeautifulSoup
+
+# def getTitle(url):
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.text, 'html.parser')
+#     title = soup.find('h1')
+#     if title:
+#         return title
+#     else:
+#         return "Title could not be found"
+
+# print(getTitle('http://www.example.com/'))
+
+# task5
+# import requests
 # import csv
 # from bs4 import BeautifulSoup as BS
-
 # def get_html(url):
 #     response = requests.get(url)
 #     return response.text
 # def get_soup(html):
 #     soup = BS(html, 'html.parser')
 #     return soup
+# def get_data(soup):
+#     s = soup.find_all('div', class_ = 'category width33 vertical-separator')
+#     # print(s)
+#     for i in s:
+#         p = i.find('span', class_='category-product-count').text
+#         print(p)
+# BASE_URL = 'https://enter.kg'
+# html = get_html(BASE_URL)
+# soup = get_soup(html)
+# get_data(soup)
 
-# def getTitle(url):
-#     html = get_html(url)
-#     soup = get_soup(html)
-#     try:
-#         a = soup.find('h1')
-#         print(a)
-#     except AttributeError:
-#         print('Title could not be found')
+# task6
+import requests
+import csv
+from bs4 import BeautifulSoup as BS
 
-# print(getTitle('http://www.example.com/'))
+def get_html(url):
+    response = requests.get(url)
+    return response.text
 
+def get_soup(html):
+    soup = BS(html, 'html.parser')
+    return soup
+title_list = []
 
-# task5
+def get_data(soup):
+    phones = soup.find_all('div', class_ = 'd-track typo-track d-track_selectable d-track_with-cover d-track_with-chart') 
+    # print(phones)
+    for phone in phones:
+        a = phone.find('div', class_='d-track__name')
+        b = phone.find('span', class_='d-track__artists')
+        img = phone.find('img', class_='entity-cover__image deco-pane')
+        img_url = img['src']
+        title = a.text.strip()
+        artist = b.text
+        image = 'https:' + img_url
+
+        write_csv({
+            'title':title,
+            'artist':artist,
+            'image':image
+        })
+
+def write_csv(data):
+    with open('music.csv', 'a') as file:
+        names = ['title', 'artist', 'image']
+        write = csv.DictWriter(file,delimiter=',',fieldnames=names)
+        write.writerow(data)
+
+# import psycopg2
+# import csv
+
+# # создайте подключение к базе данных
+# conn = psycopg2.connect("dbname=project user=postgres password=1 host=localhost")
+# # откройте CSV файл
+# with open('/Users/abdulkosim./Desktop/py25/week5/parsing/music.csv', 'r') as f:
+#     # создайте объект csv.reader и прочитайте CSV файл
+#     reader = csv.reader(f)
+#     # создайте объект курсора
+#     cursor = conn.cursor()
+#     # выполните команду COPY для копирования данных из CSV файла в таблицу
+#     cursor.copy_from(f, 'test_table', sep=',', columns=('title', 'artist', 'image'))
+#     # сохраните изменения
+#     conn.commit()
+
+def main():
+    BASE_URL = "https://music.yandex.ru/chart"
+    html = get_html(BASE_URL)
+    soup = get_soup(html)
+    get_data(soup)
+
+main()
+
+# def get_link(title_list, name):
+#     for title in title_list:
+#         if name.lower() in title['name'].lower():
+#             return title["link"]
+#     return None
+
+# print(get_link(title_list, 'Shawshank'))
